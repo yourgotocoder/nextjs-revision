c203c615f9cf0fd4ba44600df25286d7a2ea82ff
yourgotocoder <yourgotocoder@gmail.com>
2022-05-07 15:13:18 +0530

    Added get static paths for dynamically generated routes

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,15 +1,48 @@
 import React from "react";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-const SingleMeetupPage = () => {
+const SingleMeetupPage = (props) => {
     return (
         <MeetupDetail
-            title="A title"
-            image="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006.jpg/1920px-Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006.jpg"
-            description="A description"
-            address="A address"
+            title={props.meetupData.title}
+            image={props.meetupData.image}
+            description={props.meetupData.description}
+            address={props.meetupData.address}
         />
     );
 };
+
+export function getStaticProps(context) {
+    const meetupId = context.params.meetupId;
+    return {
+        props: {
+            meetupData: {
+                id: meetupId,
+                title: "A title",
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006.jpg/1920px-Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006.jpg",
+                description: "A description",
+                address: "A address",
+            },
+        },
+    };
+}
+
+export function getStaticPaths(context) {
+    return {
+        paths: [
+            {
+                params: {
+                    meetupId: "m1",
+                },
+            },
+            {
+                params: {
+                    meetupId: "m2",
+                },
+            },
+        ],
+        fallback: false,
+    };
+}
 
 export default SingleMeetupPage;
