914fb244c2d8e5b6b9c10330015e9430068f6314
yourgotocoder <yourgotocoder@gmail.com>
2022-05-07 11:14:15 +0530

    Added programmatic navigation and custom styles

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,7 +1,15 @@
 import React from "react";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 const SingleMeetupPage = () => {
-    return <div>Single Meetup Page</div>;
+    return (
+        <MeetupDetail
+            title="A title"
+            image="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006.jpg/1920px-Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006.jpg"
+            description="A description"
+            address="A address"
+        />
+    );
 };
 
 export default SingleMeetupPage;
